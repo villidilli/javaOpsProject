34ef2f16cbafd20c264cdddb84d7f175f92448d0
villidilli <e.u.kuznetsov@outlook.com>
2022-05-26 23:16:57 +0300

    New files

---
+++ TestProject.java
@@ -0,0 +1,5 @@
+public class TestProjectJava{
+	public static void main(String[] args){
+		System.out.println("Класс TestProjectJava");
+	}
+}
---
+++ TestProject2.java
@@ -0,0 +1,6 @@
+public class TestProjectJava2{
+	public static void main(String[] args){
+		System.out.println("Класс TestProjectJava2");
+		System.out.println("Класс TestProjectJava2");
+	}
+}
